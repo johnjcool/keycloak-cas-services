6b5269f682e1a17161e07762bde0531e49df32e7
Valente <rogerio.valente@capgemini.com>
2020-06-08 10:08:43 -0300

    Update to Keycloak 9.0.2
    Overrinding method on casIdentityProviderFactory

--- src/main/java/io/github/johnjcool/keycloak/broker/cas/CasIdentityProviderFactory.java
+++ src/main/java/io/github/johnjcool/keycloak/broker/cas/CasIdentityProviderFactory.java
@@ -33,4 +33,9 @@ public class CasIdentityProviderFactory extends AbstractIdentityProviderFactory<
 	public String getId() {
 		return PROVIDER_ID;
 	}
+
+  @Override
+  public <C extends IdentityProviderModel> C createConfig() {
+    return new CasIdentityProviderFactory().createConfig();
+  }
 }
